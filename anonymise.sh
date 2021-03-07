eab2d136792d140f6c4798154f97556e098f3d63
Daniel Hugenroth <daniel.hugenroth@gmail.com>
2021-03-07 13:01:22 +0000

    Removed anonymisation scripts that where used for the paper only

--- anonymise.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Replacing all occurrences of $1 with $2";
-sleep 1;
-find MobileApps -type f -exec sed -i "s/$1/$2/gi" {} +
-find SGX -type f -exec sed -i "s/$1/$2/gi" {} +
-find Simulators -type f -exec sed -i "s/$1/$2/gi" {} +
-find WebApi -type f -exec sed -i "s/$1/$2/gi" {} +
